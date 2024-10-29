d8f67e86be8a0a884c4ca1b745901389a31703fd
sungwonnoh <jessica20730@naver.com>
2024-10-29 14:59:56 +0900

    chore: api key

--- umc_movie/src/pages/now-playing.jsx
+++ umc_movie/src/pages/now-playing.jsx
@@ -11,8 +11,7 @@ export default function NowPlaying() {
         `https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1`,
         {
           headers: {
-            Authorization:
-              "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZjMyOTgyMWRmMDg1YmRmZTY3ZmNlN2Y4Nzc5ZTY0NCIsIm5iZiI6MTcyODE5NTU0MC44MzIwMzIsInN1YiI6IjY2MmQyYWViMDI4ZjE0MDEyMjY4ODI5NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.GxYapB6_nEqcdBLQDfXB1TCLvm1Z0pA9m7StOANdie0",
+            Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
           },
         }
       );
