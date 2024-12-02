7b857bba930ed9b59a00827c4b9226262a846bf5
sungwonnoh <jessica20730@naver.com>
2024-12-02 10:39:54 +0900

    feat: api연결완료

--- todolist/src/pages/main.jsx
+++ todolist/src/pages/main.jsx
@@ -51,10 +51,19 @@ const TodoCard = styled.div`
   box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
   border: 1px solid #e0e0e0;
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
   background-color: #fff;
 `;
-
+const Edit = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const EditableInput = styled.input`
+  width: 200px;
+  height: 20px;
+`;
 const TodoTitle = styled.div`
   font-size: 18px;
   font-weight: bold;
@@ -64,50 +73,99 @@ const TodoContent = styled.div`
   font-size: 14px;
   margin-bottom: 12px;
 `;
-
+const Checkbox = styled.input`
+  margin-right: 8px;
+`;
 const Btn = styled.button`
-  width: 40px;
-  height: 15px;
+  width: 70px;
+  height: 25px;
+  margin: 4px;
+  text-align: center;
 `;
 function Main() {
   const [todos, setTodos] = useState([]);
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
+  const [editId, setEditId] = useState(null);
+  const [editTitle, setEditTitle] = useState("");
+  const [editContent, setEditContent] = useState("");
 
   useEffect(() => {
-    async function Todos() {
+    async function fetchTodos() {
       try {
         const response = await axios.get("http://localhost:3000/todo");
         setTodos(response.data);
       } catch (e) {
-        console.error("errors: ", e);
+        console.error("Error fetching todos:", e);
       }
     }
-    Todos();
+    fetchTodos();
   }, []);
 
   const handleCreate = async () => {
     try {
       const newTodo = {
-        title: title,
-        content: content,
+        title,
+        content,
         checked: false,
       };
       const response = await axios.post("http://localhost:3000/todo", newTodo);
-      setTodos((prev) => [...prev, response.data]);
+      setTodos([...todos, response.data]);
       setTitle("");
       setContent("");
     } catch (e) {
-      console.error("error todo: ", e);
+      console.error("Error creating todo:", e);
+    }
+  };
+
+  const handleToggleCheck = async (id, currentChecked) => {
+    try {
+      await axios.patch(`http://localhost:3000/todo/${id}`, {
+        checked: !currentChecked,
+      });
+      setTodos((prev) =>
+        prev.map((todo) =>
+          todo.id === id ? { ...todo, checked: !currentChecked } : todo
+        )
+      );
+    } catch (e) {
+      console.error("Error toggling check:", e);
     }
   };
 
   const handleDelete = async (id) => {
+    console.log("Attempting to delete ID:", id); // 삭제할 ID를 출력
     try {
-      await axios.delete("http://localhost:3000/todo/${id}");
-      setTodos((prev) => prev.filter((todo) => todo.id !== id));
+      await axios.delete(`http://localhost:3000/todo/${id}`);
+      setTodos((prev) => prev.filter((todo) => todo.id !== id)); // 상태 업데이트
+      console.log("Delete successful");
     } catch (e) {
-      console.error("error delete: ", e);
+      console.error("Error deleting todo:", e.response ? e.response.data : e);
+    }
+  };
+
+  const handleEditToggle = (todo) => {
+    setEditId(todo.id);
+    setEditTitle(todo.title);
+    setEditContent(todo.content);
+  };
+
+  const handleEditSave = async (id) => {
+    try {
+      await axios.patch(`http://localhost:3000/todo/${id}`, {
+        title: editTitle,
+        content: editContent,
+      });
+      setTodos((prev) =>
+        prev.map((todo) =>
+          todo.id === id
+            ? { ...todo, title: editTitle, content: editContent }
+            : todo
+        )
+      );
+      setEditId(null);
+    } catch (e) {
+      console.error("Error saving edits:", e);
     }
   };
   return (
@@ -118,27 +176,51 @@ function Main() {
           placeholder="제목을 입력해주세요"
           value={title}
           onChange={(e) => setTitle(e.target.value)}
-        ></TitleInput>
+        />
         <ContentInput
           placeholder="내용을 입력해주세요"
           value={content}
           onChange={(e) => setContent(e.target.value)}
-        ></ContentInput>
+        />
         <Button onClick={handleCreate}>todo 생성하기</Button>
       </TodoMaking>
       <TodoContainer>
-        {todos.map((todo, index) => (
-          <TodoCard key={index}>
-            <TodoTitle>{todo.title}</TodoTitle>
-            <TodoContent>{todo.content}</TodoContent>
-            <div>
-              <Btn>수정</Btn>
-              <Btn danger onClick={() => handleDelete(todo.id)}>
-                삭제
-              </Btn>
-            </div>
-          </TodoCard>
-        ))}
+        {todos.map((todo) =>
+          editId === todo.id ? (
+            <TodoCard key={todo.id}>
+              <Edit>
+                <EditableInput
+                  value={editTitle}
+                  onChange={(e) => setEditTitle(e.target.value)}
+                />
+                <EditableInput
+                  value={editContent}
+                  onChange={(e) => setEditContent(e.target.value)}
+                />
+              </Edit>
+              <div>
+                <Btn onClick={() => handleEditSave(todo.id)}>수정완료</Btn>
+              </div>
+            </TodoCard>
+          ) : (
+            <TodoCard key={todo.id}>
+              <Checkbox
+                type="checkbox"
+                checked={todo.checked}
+                onChange={() => handleToggleCheck(todo.id, todo.checked)}
+              />
+              <div>
+                <TodoTitle>{todo.title}</TodoTitle>
+                <TodoContent>{todo.content}</TodoContent>
+              </div>
+
+              <div>
+                <Btn onClick={() => handleEditToggle(todo)}>수정</Btn>
+                <Btn onClick={() => handleDelete(todo.id)}>삭제</Btn>
+              </div>
+            </TodoCard>
+          )
+        )}
       </TodoContainer>
     </Wrapper>
   );
